eba9b4f5ee21c0b9404e644152b9d511922a98f5
alekhaya99 <alekhaya.datta@gmail.com>
2020-11-18 23:28:01 +0800

    new update

--- ExternalJS/JS_TUT1.js
+++ ExternalJS/JS_TUT1.js
@@ -3,6 +3,22 @@ console.log("TUTORIAL");
 function hello(variable){
     console.log("Hello"+variable)
 }
-
+function yo(variable,text1){
+    console.log("Hello"+variable+text1)
+}
 hello("world");
 hello("Alek");
+
+yo("world","bro")
+//Return type function
+function subtraction(a,b){
+    let c=a-b;
+    return c;
+}
+ let r=subtraction(1,3);
+ console.log(r);
+
+
+
+
+

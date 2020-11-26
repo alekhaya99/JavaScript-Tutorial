1243ff179bbb89c00bc475e061404ff4cee24d58
alekhaya99 <alekhaya.datta@gmail.com>
2020-11-26 20:56:11 +0800

    new update

---
+++ NodeJS/index.js
@@ -0,0 +1,19 @@
+const http=require('http');
+const fs=require('fs');
+
+const hostname='127.0.0.1';
+const port=3000;
+
+const home = fs.readFileSync('NodeJS/index.html')
+const about = fs.readFileSync('NodeJS/about.html')
+
+const server=http.createServer((req,res)=>{
+    console.log(req.url)
+    res.statusCode=200;
+    res.setHeader('Content-type','text/html');
+    res.end(home);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})

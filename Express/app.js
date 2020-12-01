f1cb75cac1c737dbaa71562d5d116ef8b898afe7
alekhaya99 <alekhaya.datta@gmail.com>
2020-12-01 23:06:33 +0800

    update

--- Express/app.js
+++ Express/app.js
@@ -9,22 +9,28 @@ app.set('view engine','pug');
 
 app.set('views',path.join(__dirname,'views'));
 
-app.get("/demo",(req,res)=>{
-    res.status(200).render('demo',{title:'Hello',message:"Hello World"})
-});
-
-
 app.get("/",(req,res)=>{
-    res.status(200).send("THIS IS MY FIRST EXPRESS APP TUTORIAL")
+    const conten="This is an example of a content page"
+    const parameters= {'title':'I am Alek','content':conten};
+    res.status(200).render('index.pug',parameters)
 });
 
-app.get("/about",(req,res)=>{
-    res.send("THIS IS MY FIRST EXPRESS APP TUTORIAL IN ABOUT")
-})
+// app.get("/demo",(req,res)=>{
+//     res.status(200).render('demo',{title:'Hello',message:"Hello World"})
+// });
 
-app.post("/post",(req,res)=>{
-    res.send("THIS IS MY FIRST EXPRESS APP TUTORIAL IN POST")
-})
+
+// app.get("/",(req,res)=>{
+//     res.status(200).send("THIS IS MY FIRST EXPRESS APP TUTORIAL")
+// });
+
+// app.get("/about",(req,res)=>{
+//     res.send("THIS IS MY FIRST EXPRESS APP TUTORIAL IN ABOUT")
+// })
+
+// app.post("/post",(req,res)=>{
+//     res.send("THIS IS MY FIRST EXPRESS APP TUTORIAL IN POST")
+// })
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)

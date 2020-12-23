0e48d4ed8ee8f795ef75e478e990302c2f833350
alekhaya99 <alekhaya.datta@gmail.com>
2020-12-23 22:48:31 +0800

    Hello

---
+++ MongoDB/mongo.js
@@ -0,0 +1,10 @@
+// Inserting data in mongo db
+use alekcart
+db.alekcart
+db.items.insertOne({
+    name:"ALek",roll:32,id:4125,class:5
+})
+db.items.insertMany([{
+    name:"ALek",roll:32,id:4125,class:5
+},{name:"ALek2",roll:33,id:4126,class:5
+}])

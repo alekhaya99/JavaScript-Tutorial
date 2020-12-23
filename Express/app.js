526a74d15834b6177a276d0c87fb2c34b570d8bd
alekhaya99 <alekhaya.datta@gmail.com>
2020-12-23 20:55:25 +0800

    Update

--- Express/app.js
+++ Express/app.js
@@ -1,12 +1,14 @@
 const express=require("express");
 const path=require("path");
+const fs = require("fs");
 const app=express();
-const port=8000
+const port=8000;
 
 app.use('./static',express.static('static'));
+app.use(express.urlencoded())
 
 app.set('view engine','pug');
-
+  
 app.set('views',path.join(__dirname,'views'));
 
 app.get("/",(req,res)=>{
@@ -14,6 +16,23 @@ app.get("/",(req,res)=>{
     const parameters= {'title':'I am Alek','content':conten};
     res.status(200).render('index.pug',parameters)
 });
+app.post('/', (req, res)=>{
+    name = req.body.name
+    date = req.body.date
+    who = req.body.who
+    address = req.body.address
+    more = req.body.more
+    let outputToWrite = `the name of the client is ${name}, ${date} is the date, ${who}, residing at ${address}. More about him/her: ${more}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+
+})
+// app.post("/",(req,res)=>{
+//     console.log(req.body)
+//     const parameters= {'message':'Your form has been submitted'};
+//     res.status(200).render('index.pug',parameters)
+// })
 
 // app.get("/demo",(req,res)=>{
 //     res.status(200).render('demo',{title:'Hello',message:"Hello World"})
